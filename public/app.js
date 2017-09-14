5dbd63670184e072907fbb1bd04da71fa08923a7
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-14 17:59:32 -0600

    got front and back talking

--- public/app.js
+++ public/app.js
@@ -1,1 +1,14 @@
 console.log('app.js is working');
+
+const app = angular.module('workout', []);
+
+app.controller('mainController', ['$http', function($http) {
+  this.message = "controller works";
+
+  $http({
+    method: 'GET',
+    url: 'http://localhost:3000/bodies',
+  }).then(response => console.log(response))
+    .catch(err => console.log(err));
+
+}]);
