bb86a596a7d78040587ef528521263ad221c506b
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-09-15 15:18:15 -0600

    exercises and muscles showing

--- public/app.js
+++ public/app.js
@@ -3,13 +3,16 @@ console.log('app.js is working');
 const app = angular.module('workout_app', []);
 
 app.controller('mainController', ['$http', function($http) {
+  const controller = this;
   this.message = "controller works";
   this.url = 'http://localhost:3000';
   this.user = {};
+  this.users = [];
 
+// ===================== User-related =========================
+// user login
   this.login = function(userPass) {
     console.log(userPass);
-
     $http({
       method: 'POST',
       url: this.url + '/users/login',
@@ -21,6 +24,7 @@ app.controller('mainController', ['$http', function($http) {
     }.bind(this));
   }
 
+// get user
   this.getUsers = function() {
     $http({
       url: this.url + '/users',
@@ -38,6 +42,7 @@ app.controller('mainController', ['$http', function($http) {
     }.bind(this));
   }
 
+// logout user
   this.logout = function() {
     localStorage.clear('token');
     location.reload();
@@ -48,7 +53,36 @@ app.controller('mainController', ['$http', function($http) {
   // }).then(response => console.log(response))
   //   .catch(err => console.log(err));
 
+ // ==================== End User-related =======================
 
+// ===================== Body-related ==========================
 
+// get all bodies
+this.getBodies = function() {
+  $http({
+    url: this.url + '/bodies',
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response.data);
+    controller.body = response.data;
+  })
+}
+
+// ===================== End Body-related ==========================
+
+// ===================== Exercise-related ==========================
+
+// get all bodies
+this.getExercises = function() {
+  $http({
+    url: this.url + '/exercises',
+    method: 'GET'
+  }).then(function(response) {
+    console.log(response.data);
+    controller.exercise = response.data;
+  })
+}
+
+// ===================== End Exercise-related ==========================
 
 }]);
